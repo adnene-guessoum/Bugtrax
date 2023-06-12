adf39c617fb8964fe1f81ab445806dc738ecfbab
Adnene Guessoum <adnen.guessoum@gmail.com>
2023-06-12 14:37:16 +0200

    Ajout: Lier middleware à app (auth, users, tickets)
    - Pour l'instant: vides

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
 
-const hostname = '0.0.0.0';
-const port = 8000;
+const hostname = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tickets', require('./routes/tickets'));
 
 app.get('/', (req: Request, res: Response) => {
   console.log(req.headers);
