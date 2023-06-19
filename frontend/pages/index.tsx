14fbf15bf30d55ff5a1b6e3afa41d488c16dd186
Adnene Guessoum <adnen.guessoum@gmail.com>
2023-06-19 14:11:50 +0200

    fix frontend issues
    - simplify layout by removing dashboard
    - change description from other project to align with this one

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Link from 'next/link';
 import Layout from '../components/Layout';
 
 const IndexPage = (): JSX.Element => {
   return (
     <Layout>
-      <p>En construction</p>
+      <div className="flex flex-col gap-4 justify-center items-center mt-12">
+        <h2 className="underline font-semibold text-2xl">
+          Veuillez vous connecter
+        </h2>
+        <Link href="/login">
+          <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Connexion
+            </button>
+          </div>
+        </Link>
+
+        <Link href="/register">
+          <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Inscription
+            </button>
+          </div>
+        </Link>
+      </div>
     </Layout>
   );
 };
